e82bd73d7ede29d66112d2e251338bcc32dfb998
Le Duy Khanh <ledu0003@e.ntu.edu.sg>
2018-05-04 14:28:40 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+How to run:
+
+- Install Java
+- Modify input.in for stream of input
+- cd tenx-test
+- javac TenXTest.java
+- java TenXTest < input.in
--- TenXTest.java
+++ TenXTest.java
@@ -19,49 +19,56 @@ class TenXTest {
 			String line = sc.nextLine();
 			String items[] = line.split(" ");
 			
-			//Exchange Rate Requests
-			if(line.contains("EXCHANGE_RATE_REQUEST")) {
-				String source_exchange = items[1];
-				String source_currency = items[2];
-				String destination_exchange = items[3];
-				String destination_currency = items[4];
-				Vertex u = new Vertex(source_exchange, source_currency);
-				Vertex v = new Vertex(destination_exchange, destination_currency);
-				Key uv = new Key(u, v);
-				bestRates();
-				System.out.println("BEST_RATES_BEGIN " + source_exchange + " " + source_currency 
-						+ " " + destination_exchange + " " + destination_currency + " " + rate.get(uv));			
-				printPath(path(u, v));
-				System.out.println("BEST_RATES_END");
-
- 			}
+			try {
 			
-			else { // Price updates
-				String exchange = items[1];
-				String source_currency = items[2];
-				String destination_currency = items[3];
-				float forward_factor = Float.parseFloat(items[4]);
-				float backward_factor = Float.parseFloat(items[5]);
-				Vertex v1 = new Vertex(exchange, source_currency);
-				Vertex v2 = new Vertex(exchange, destination_currency);
-				Edge e1 = new Edge(v1, v2, forward_factor);
-				Edge e2 = new Edge(v2, v1, backward_factor);
+				//Exchange Rate Requests
+				if(line.contains("EXCHANGE_RATE_REQUEST")) {
+					String source_exchange = items[1];
+					String source_currency = items[2];
+					String destination_exchange = items[3];
+					String destination_currency = items[4];
+					Vertex u = new Vertex(source_exchange, source_currency);
+					Vertex v = new Vertex(destination_exchange, destination_currency);
+					Key uv = new Key(u, v);
+					bestRates();
+					System.out.println("BEST_RATES_BEGIN " + source_exchange + " " + source_currency 
+							+ " " + destination_exchange + " " + destination_currency + " " + rate.get(uv));			
+					printPath(path(u, v));
+					System.out.println("BEST_RATES_END");
+	
+	 			}
 				
-				for(Vertex v : vertices) {
-					if (v.getCurrency().equals(source_currency)) {
-						graph.add(new Edge(v1, v, 1));
-						graph.add(new Edge(v, v1, 1));
-					}
+				else { // Price updates
+					String exchange = items[1];
+					String source_currency = items[2];
+					String destination_currency = items[3];
+					float forward_factor = Float.parseFloat(items[4]);
+					float backward_factor = Float.parseFloat(items[5]);
+					Vertex v1 = new Vertex(exchange, source_currency);
+					Vertex v2 = new Vertex(exchange, destination_currency);
+					Edge e1 = new Edge(v1, v2, forward_factor);
+					Edge e2 = new Edge(v2, v1, backward_factor);
 					
-					if (v.getCurrency().equals(destination_currency)) {
-						graph.add(new Edge(v2, v, 1));
-						graph.add(new Edge(v, v2, 1));
+					for(Vertex v : vertices) {
+						if (v.getCurrency().equals(source_currency)) {
+							graph.add(new Edge(v1, v, 1));
+							graph.add(new Edge(v, v1, 1));
+						}
+						
+						if (v.getCurrency().equals(destination_currency)) {
+							graph.add(new Edge(v2, v, 1));
+							graph.add(new Edge(v, v2, 1));
+						}
 					}
+					graph.add(e1);
+					graph.add(e2);
+					vertices.add(v1);
+					vertices.add(v2);
 				}
-				graph.add(e1);
-				graph.add(e2);
-				vertices.add(v1);
-				vertices.add(v2);
+			}
+		
+			catch(Exception e) {
+				System.out.println("Wrong input format");
 			}
 		}
 	}
